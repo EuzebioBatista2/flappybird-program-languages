920fbc0d73e1bfb0f636c452ece99ea1cb3f2729
Euzebio Batista <euzebio.batista2@gmail.com>
2023-10-14 16:32:25 -0300

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    // use AuthenticatesUsers;
 
     /**
      * Where to redirect users after login.
@@ -42,7 +42,7 @@ class LoginController extends Controller
     {
     // Valide os dados de entrada do formulário de login
     $request->validate([
-        'email' => 'required|email',
+        'name' => 'required',
         'password' => 'required',
     ]);
 
