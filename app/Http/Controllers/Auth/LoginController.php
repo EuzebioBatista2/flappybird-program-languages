a47c2093cc979fdd396acadc2d1143ee3699e680
Euzebio Batista <euzebio.batista2@gmail.com>
2023-10-14 16:27:47 -0300

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -37,4 +37,19 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function login(Request $request)
+    {
+    // Valide os dados de entrada do formulário de login
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    if (Auth::attempt(['name' => $request->name, 'password' => $request->password])) {
+        return redirect('/home');
+    } else {
+        return back()->with('error', 'Credenciais inválidas. Tente novamente.'); // Redirecionar de volta à página de login com uma mensagem de erro
+    }
+}
 }
