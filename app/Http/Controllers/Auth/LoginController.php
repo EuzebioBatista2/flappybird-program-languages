d6a138afcba9c79983aad8c1e35b42ef918e0638
Euzebio Batista <Euzebio.batista2@gmail.com>
2023-10-15 08:07:25 -0300

    Update function Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
+
 namespace Illuminate\Foundation\Auth;
 
 use App\Http\Controllers\Controller;
@@ -15,7 +16,7 @@ use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
-    /*
+  /*
     |--------------------------------------------------------------------------
     | Login Controller
     |--------------------------------------------------------------------------
@@ -26,23 +27,97 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+  use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+  /**
+   * Where to redirect users after login.
+   *
+   * @var string
+   */
+  protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('guest')->except('logout');
+  }
+
+  public function newlogin(Request $request)
+  {
+    $this->validateLogin($request);
+
+    // If the class is using the ThrottlesLogins trait, we can automatically throttle
+    // the login attempts for this application. We'll key this by the username and
+    // the IP address of the client making these requests into this application.
+    if (
+      method_exists($this, 'hasTooManyLoginAttempts') &&
+      $this->hasTooManyLoginAttempts($request)
+    ) {
+      $this->fireLockoutEvent($request);
+
+      return $this->sendLockoutResponse($request);
     }
 
+    if ($this->attemptLogin($request)) {
+      if ($request->hasSession()) {
+        $request->session()->put('auth.password_confirmed_at', time());
+      }
+
+      return $this->sendLoginResponse($request);
+    }
+
+    // If the login attempt was unsuccessful we will increment the number of attempts
+    // to login and redirect the user back to the login form. Of course, when this
+    // user surpasses their maximum number of attempts they will get locked out.
+    $this->incrementLoginAttempts($request);
+
+    return $this->sendFailedLoginResponse($request);
+  }
+
+  protected function validateLogin(Request $request)
+  {
+    $request->validate(
+      [
+        'name' => 'required|string|exists:users,name',
+        'password' => 'required|string',
+      ],
+      [
+        "name.required" => 'O campo nome precisa ser preenchido',
+        "password.required" => 'O campo senha deve ser preenchido',
+        "name.exists" => 'Login ou senha inválidos'
+      ]
+    );
+  }
+
+  protected function attemptLogin(Request $request)
+  {
+    return $this->guard()->attempt(
+      $this->credentials($request),
+      $request->boolean('remember')
+    );
+  }
+
+  protected function sendLoginResponse(Request $request)
+  {
+    $request->session()->regenerate();
+
+    $this->clearLoginAttempts($request);
+
+    if ($response = $this->authenticated($request, $this->guard()->user())) {
+      return $response;
+    }
+
+    return $request->wantsJson()
+      ? new JsonResponse([], 204)
+      : redirect()->intended($this->redirectPath());
+  }
+
+  public function username()
+  {
+    return 'name';
+  }
 }
